8d46d7d338df3fd8cfd8c138223dff8c5c6b2275
lysovath <sovathngov1@gmail.com>
2026-05-22 13:46:12 +0700

    Submit EX-2

--- EX-2/server.js
+++ EX-2/server.js
@@ -1,5 +1,5 @@
 // server.js
-import express from ('express');
+import express from 'express';
 import courses from "./course.js";
 const app = express();
 const PORT = 3000;
@@ -10,6 +10,20 @@ app.get('/departments/:dept/courses', (req, res) => {
     const { level, minCredits, maxCredits, semester, instructor } = req.query;
     // Implementing the filter logic
     // Hint: Use the filter method to filter the courses array based on the provided criteria
+    const coursesFiltered = courses.filter(course => {
+        if(course.department.toLowerCase() !== dept) return false;
+        if(level && course.level.toLowerCase() !== level) return false;
+        if(minCredits && course.credits < Number(minCredits)) return false;
+        if(maxCredits && course.credits > Number(maxCredits)) return false;
+        if(semester && course.semester.toLowerCase() !== semester) return false;
+        if(instructor && course.instructor.toLowerCase() !== instructor) return false;
+        return true;
+    })
+    if(coursesFiltered.length === 0){
+        return res.send(`No data found`);
+    }
+    return res.send(coursesFiltered);
+
 });
 
 app.listen(PORT, () => {
